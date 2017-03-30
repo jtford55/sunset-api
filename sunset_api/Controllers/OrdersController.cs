0c0d5d5669501e4a739d54121cbc4fedf6bc8ac2
Jason Ford <jason@scaleit.net>
2017-03-30 15:06:34 -0400

    Qurries built in.

--- sunset_api/Controllers/OrdersController.cs
+++ sunset_api/Controllers/OrdersController.cs
@@ -8,7 +8,9 @@ namespace sunset_api.Controllers
 {
     [Route("api/[controller]")]
     public class OrdersController : Controller
-    {        
+    {
+        private string result = string.Empty;
+
         // GET api/orders
         [HttpGet]
         public IEnumerable<string> Get()
@@ -19,16 +21,13 @@ namespace sunset_api.Controllers
         //// Get started order based on ID
         //// GET api/orders/<order-id>/started
         [HttpGet("{id}/started")]
-        public string Get(int id)
+        public void Get(int id)
         {
             //Writes a single column on a table.  Write 'STD' to the orderheader.ord_status column   where the orderheader.ord_number == the id in the GET url
             //Returns nothing, 200 OK
 
             DBAccess db = new DBAccess();
-            string results = db.Querry("SELECT * FROM Orders WHERE id = " + id);
-            
-            
-            return "Started Order: " + results;
+            db.Update("UDPATE orderheader SET ord_status = 'STD' WHERE orderheader.ord_number = " + id.ToString());           
         }
 
         //// GET api/orders/byStatus?status="PLN"&start=2017-03-06T19:07:46Z&end=2017-03-07T23:07:46Z
@@ -36,20 +35,35 @@ namespace sunset_api.Controllers
         public string Get(string status, DateTime start, DateTime end)
         {
             //Querry DB for data and return JSON
+            /*
+             SELECT orderheader.ord_number AS orderheader_ord_number, orderheader.ord_status AS orderheader_ord_status, orderheader.ord_totalweight AS orderheader_ord_totalweight, orderheader.ord_refnum AS orderheader_ord_refnum, orderheader.ord_description AS orderheader_ord_description, orderheader.ord_driver1 AS orderheader_ord_driver1, orderheader.ord_tractor AS orderheader_ord_tractor 
+             FROM orderheader JOIN stops ON orderheader.ord_number = stops.ord_hdrnumber AND stops.stp_type = %(stp_type_1)s 
+             WHERE orderheader.ord_status = %(ord_status_1)s AND stops.stp_schdtearliest > %(stp_schdtearliest_1)s AND stops.stp_schdtearliest < %(stp_schdtearliest_2)s
+            */
+            DBAccess db = new DBAccess();
 
-            return status + " " + start.ToString() + " " + end.ToString() ;
+            string query = "SELECT orderheader.ord_number AS orderheader_ord_number, orderheader.ord_status AS orderheader_ord_status, orderheader.ord_totalweight AS orderheader_ord_totalweight," +
+                           "orderheader.ord_refnum AS orderheader_ord_refnum, orderheader.ord_description AS orderheader_ord_description, orderheader.ord_driver1 AS orderheader_ord_driver1," +
+                           "orderheader.ord_tractor AS orderheader_ord_tractor FROM orderheader JOIN stops ON orderheader.ord_number = stops.ord_hdrnumber AND stops.stp_type = %(stp_type_1)s " +
+                           "WHERE orderheader.ord_status = '" + status + "' AND stops.stp_schdtearliest > '" + start.ToString() + "' AND stops.stp_schdtearliest < '" + end.ToString() + "'";
+
+            result = db.QuerryJSON(query);
+
+            return result;
         }
 
 
         //// GET api/orders/<order-id>/complete/?weight="22.34"&ticket_number="1938429"
         [HttpGet("{id}/complete")]
-        public string Get(int id, string weight, string ticket_number)
+        public void Get(int id, string weight, string ticket_number)
         {
             //Writes 'CMP' to the orderheader.ord_status column, weight to orderheader.ord_weight, ticket number to orderheader.ord_refnum
             //And also writes the weight to the stops.stp_weight where the stops.ord_hdrnumber is the order - id and the stops.stp_type = DRP
             //Returns nothing, 200 OK
 
-            return id.ToString() + " "  + weight + " " + ticket_number;
+            DBAccess db = new DBAccess();
+            db.Update("UDPATE orderheader SET ord_status = 'CMP', ord_weight = '" + weight + "', ord_refnum = '" + ticket_number + "' WHERE orderheader.ord_number = " + id.ToString());
+            db.Update("UPDATE stops SET stp_weight = '" + weight + "' WHERE ord_hdrnumber = '" + id.ToString() + "' AND stp_type = 'DRP'");
         }
     }
 }
--- sunset_api/DBAccess.cs
+++ sunset_api/DBAccess.cs
@@ -11,9 +11,7 @@ namespace sunset_api
     {        
         public SqlConnection conn = new SqlConnection(@"Data Source=.\WSSERVER;Initial Catalog=WeighingSystem;User id=sa;Password=rs232cv;");
 
-        private System.Xml.XmlReader xmlData;
-
-        public string Querry(string querry)
+        public string QuerryJSON(string querry)
         {
             string results = string.Empty;
             conn.Open();
@@ -23,15 +21,43 @@ namespace sunset_api
             command.CommandText = querry;
             SqlDataReader reader = command.ExecuteReader();
 
-            string jsonResult = string.Empty;
-            while (reader.Read())
-            {
-                jsonResult += JsonConvert.SerializeObject(reader.ToString());
-            }                       
+            var r = Serialize(reader);
+            string json = JsonConvert.SerializeObject(r, Formatting.Indented);
 
             conn.Close();
-            return jsonResult;
+            return json;
         }
-     
+
+        public void Update(string querry)
+        {
+            string results = string.Empty;
+            conn.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.Connection = conn;
+            command.CommandText = querry;
+            command.ExecuteNonQuery();            
+        }
+
+        private IEnumerable<Dictionary<string, object>> Serialize(SqlDataReader reader)
+        {
+            var results = new List<Dictionary<string, object>>();
+            var cols = new List<string>();
+            for (var i = 0; i < reader.FieldCount; i++)
+                cols.Add(reader.GetName(i));
+
+            while (reader.Read())
+                results.Add(SerializeRow(cols, reader));
+
+            return results;
+        }
+        private Dictionary<string, object> SerializeRow(IEnumerable<string> cols,SqlDataReader reader)
+        {
+            var result = new Dictionary<string, object>();
+            foreach (var col in cols)
+                result.Add(col, reader[col]);
+            return result;
+        }
+
     }
 }
